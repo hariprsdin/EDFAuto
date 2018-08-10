d8523d4e721579c1f4d8c14aa089982fe5fa5483
hariprsdin <hariprsdin@gmail.com>
2018-08-10 11:23:16 +0530

    Tet

--- src/test/java/com/macrosoftinc/automation/LoginTest.java
+++ src/test/java/com/macrosoftinc/automation/LoginTest.java
@@ -8,19 +8,16 @@ import org.testng.annotations.Test;
 public class LoginTest {
 
 	@Test
-	public static void LoginClient() throws Exception {
-		try {
-			System.setProperty("webdriver.chrome.driver", "D://SafetyFirst/chromedriver/chromedriver.exe");
-			WebDriver driver = new ChromeDriver();
-			driver.get("https://www.google.co.in/");
-			driver.manage().window().maximize();
-			System.out.println("Welcome to Jenkins");
-			Thread.sleep(3000);
-			//Assert.assertEquals("Log In – Perficient Wiki", driver.getTitle());
-			driver.quit();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+	public static void LoginClient() {
+
+		System.setProperty("webdriver.chrome.driver", "D://SafetyFirst/chromedriver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		driver.manage().window().maximize();
+		System.out.println("Welcome to Jenkins");
+		// Thread.sleep(3000);
+		// Assert.assertEquals("Log In – Perficient Wiki", driver.getTitle());
+		driver.quit();
 
 	}
 }
