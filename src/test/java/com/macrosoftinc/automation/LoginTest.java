e2f933a1f089e206b2f21d217f52780a43c265d9
hariprsdin <hariprsdin@gmail.com>
2018-08-13 12:20:01 +0530

    Test Commit

--- src/test/java/com/macrosoftinc/automation/LoginTest.java
+++ src/test/java/com/macrosoftinc/automation/LoginTest.java
@@ -33,7 +33,7 @@ public static WebDriver driver;
 		driver.findElement(By.id("userName")).sendKeys("hprasad");
 		driver.findElement(By.id("password")).sendKeys("Test@123");
 		driver.findElement(By.cssSelector(".btn.btn-login")).click();
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		driver.close();
 	}
 	
