69da5f29abb57f6b7f43f8b1391e1e85c87ed320
hariprsdin <hariprsdin@gmail.com>
2018-08-14 10:34:42 +0530

    test

--- src/test/java/com/macrosoftinc/automation/LoginTest.java
+++ src/test/java/com/macrosoftinc/automation/LoginTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class LoginTest {
 public static WebDriver driver;
 	@Test
-	public static void LoginCase1() throws Exception{
+	public static void Valid() throws Exception{
 
 		System.out.println(" Executing SAF urls");
 		System.setProperty("webdriver.chrome.driver", "F:\\selenium-files\\chromedriver.exe");
@@ -26,7 +26,7 @@ public static WebDriver driver;
 		driver.close();
 	}
 	@Test
-	public static void LoginCase2() throws Exception {
+	public static void Invalid() throws Exception {
 
 		System.out.println(" Executing SAF urls");
 		System.setProperty("webdriver.chrome.driver", "F:\\selenium-files\\chromedriver.exe");
@@ -36,7 +36,7 @@ public static WebDriver driver;
 		driver.findElement(By.id("userName")).sendKeys("hprasad");
 		driver.findElement(By.id("password")).sendKeys("Test@123");
 		driver.findElement(By.cssSelector(".btn.btn-login")).click();
-		Thread.sleep(1000);
+		Thread.sleep(5000);
 		String actualTitle = driver.getTitle();
 		String expectedTitle = "My Title";
 		//assertTrue(driver.findElement(By.id("Test")).isEnabled());
